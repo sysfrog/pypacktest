6eb51a60b29a782862118cf4b28d4c81490bbd02
Thomas Heggelund <thomas_heggelund_rocketfarm@Thomass-MacBook-Pro.local>
2017-11-08 13:56:11 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pypacktest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/sysfrog/pypacktest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
